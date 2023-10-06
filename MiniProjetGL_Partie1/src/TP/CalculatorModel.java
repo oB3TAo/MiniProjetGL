fb37462fb52c343dc2b6b18a2e864a557692b325
oB3TAo <loic-laine@orange.fr>
2023-10-06 14:56:29 +0200

    Test code premiere partie

--- MiniProjetGL_Partie1/src/TP/CalculatorModel.java
+++ MiniProjetGL_Partie1/src/TP/CalculatorModel.java
@@ -1,5 +1,75 @@
 package TP;
 
-public class CalculatorModel {
+import java.util.Stack;
 
+//Modèle de la calculatrice
+public class CalculatorModel {
+ private double accumulator; // L'accumulateur
+ private Stack<Double> operandStack; // La pile d'opérandes
+
+ public CalculatorModel() {
+     accumulator = 0.0;
+     operandStack = new Stack<>();
+ }
+
+ // Méthode pour effectuer une opération d'addition
+ public void add() {
+     if (!operandStack.isEmpty()) {
+         double operand = operandStack.pop();
+         accumulator += operand;
+     }
+ }
+
+ // Méthode pour effectuer une opération de soustraction
+ public void subtract() {
+     if (!operandStack.isEmpty()) {
+         double operand = operandStack.pop();
+         accumulator -= operand;
+     }
+ }
+
+ // Méthode pour effectuer une opération de multiplication
+ public void multiply() {
+     if (!operandStack.isEmpty()) {
+         double operand = operandStack.pop();
+         accumulator *= operand;
+     }
+ }
+
+ // Méthode pour effectuer une opération de division
+ public void divide() {
+     if (!operandStack.isEmpty()) {
+         double operand = operandStack.pop();
+         if (operand != 0) {
+             accumulator /= operand;
+         } else {
+             // Gérer la division par zéro ici si nécessaire
+         }
+     }
+ }
+
+ // Méthode pour empiler un nombre sur la pile
+ public void push(double value) {
+     operandStack.push(value);
+ }
+
+ // Méthode pour vider la pile
+ public void clearStack() {
+     operandStack.clear();
+ }
+
+ // Méthode pour vider l'accumulateur
+ public void clearAccumulator() {
+     accumulator = 0.0;
+ }
+
+ // Méthode pour obtenir la valeur actuelle de l'accumulateur
+ public double getAccumulator() {
+     return accumulator;
+ }
+
+ // Méthode pour obtenir la pile d'opérandes (pour des besoins de débogage, par exemple)
+ public Stack<Double> getOperandStack() {
+     return operandStack;
+ }
 }
