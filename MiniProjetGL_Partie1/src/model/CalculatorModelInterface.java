2589ea4858367c8b54c9b029d0c6d1fb2e54c86b
clemg <clemg@159.31.68.139>
2023-10-13 15:16:00 +0200

    Déclaration des méthodes dans l'interface CM, définition de méthodes

--- MiniProjetGL_Partie1/src/model/CalculatorModelInterface.java
+++ MiniProjetGL_Partie1/src/model/CalculatorModelInterface.java
@@ -2,4 +2,41 @@ package model;
 
 public interface CalculatorModelInterface {
 
+	// Méthode pour effectuer une opération de soustraction
+	public void add() {}
+
+	// Méthode pour effectuer une opération de soustraction
+	public void subtract() {}
+
+	// Méthode pour effectuer une opération de multiplication
+	public void multiply() {}
+
+	// Méthode pour effectuer une opération de division
+	public void divide() {}
+
+	// Méthode pour empiler un nombre sur la pile
+	public void push(double value) {}
+
+	// Méthode pour prendre le premier élémenet de la pile
+	public void pop() {}
+
+	// Méthode drop
+	public void drop() {}
+
+	// Méthode swap
+	public void swap() {}
+
+	// Méthode pour vider la pile
+	public void clearStack() {}
+
+	// Méthode pour vider l'accumulateur
+	public void clearAccumulator() {}
+
+	// Méthode pour obtenir la valeur actuelle de l'accumulateur
+	public double getAccumulator() {}
+
+	// Méthode pour obtenir la pile d'opérandes (pour des besoins de débogage, par
+	// exemple)
+	public Stack<Double> getOperandStack() {}
+
 }
