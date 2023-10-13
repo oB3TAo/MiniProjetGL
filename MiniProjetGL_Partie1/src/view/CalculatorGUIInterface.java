11eb9cf36317fe5b8163de8f56cbdf7a5d813263
oB3TAo <loic-laine@orange.fr>
2023-10-13 15:02:00 +0200

    Ajout dans calculatorGUI des méthode

--- MiniProjetGL_Partie1/src/view/CalculatorGUIInterface.java
+++ MiniProjetGL_Partie1/src/view/CalculatorGUIInterface.java
@@ -2,4 +2,15 @@ package view;
 
 public interface CalculatorGUIInterface {
 
+	public void affiche() {
+	     
+	     }
+	public void change(String accu) {
+	     
+    }
+	public void change(List<Double> stackData) {
+	     
+    }
+	 }	
+	
 }
