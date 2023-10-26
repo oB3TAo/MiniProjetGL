759f36888ee35609d5833f701f7befe1de04ce58
clemg <clemg@10.134.33.236>
2023-10-26 08:17:00 +0200

    Modifications apportées en vue des rmq recettes

--- MiniProjetGL_Partie1/src/controler/CalculatorControler.java
+++ MiniProjetGL_Partie1/src/controler/CalculatorControler.java
@@ -24,15 +24,14 @@ public class CalculatorControler implements CalculatorControlerInterface {
 		
 		accu = "";
 		stackData = new ArrayList<Double>();
-		stackData.add(0.0);
-		stackData.add(0.0);
+		
 		stage = primaryStage;
 		gui = new CalculatorGUI(stage);
 		calc = new CalculatorModel();
 		gui.affiche();
 		gui.change(accu);
 		gui.change(stackData);
-		for (int i = 0; i < 16; i++) {
+		for (int i = 0; i < 20; i++) {
 			gui.getButton(i).setOnAction(new ButtonHandler());
 		}
 		
@@ -62,113 +61,64 @@ public class CalculatorControler implements CalculatorControlerInterface {
 			String buttonText = source.getText();
 			
 			if (buttonText.equals("1")) {
-				stackData.add(0,1.0);
-				change(stackData);
-				calc.push(1.0);
-				System.out.println(calc.getOperandStack());
+				change("1");
 			} else if (buttonText.equals("2")){
-				stackData.add(0,2.0);
-				change(stackData);
-				calc.push(2.0);
-				System.out.println(calc.getOperandStack());
+				change("2");
 			} else if (buttonText.equals("3")){
-				stackData.add(0,3.0);
-				change(stackData);
-				calc.push(3.0);
-				System.out.println(calc.getOperandStack());
+				change("3");
 			} else if (buttonText.equals("4")){
-				stackData.add(0,4.0);
-				change(stackData);
-				calc.push(4.0);
-				System.out.println(calc.getOperandStack());
+				change("4");
 			} else if (buttonText.equals("5")){
-				stackData.add(0,5.0);
-				change(stackData);
-				calc.push(5.0);
-				System.out.println(calc.getOperandStack());
+				change("5");
 			} else if (buttonText.equals("6")){
-				stackData.add(0,6.0);
-				change(stackData);
-				calc.push(6.0);
-				System.out.println(calc.getOperandStack());
+				change("6");
 			} else if (buttonText.equals("7")){
-				stackData.add(0,7.0);
-				change(stackData);
-				calc.push(7.0);
-				System.out.println(calc.getOperandStack());
-				
+				change("7");
 			} else if (buttonText.equals("8")){
-				stackData.add(0,8.0);
-				change(stackData);
-				calc.push(8.0);
-				System.out.println(calc.getOperandStack());
+				change("8");
 			} else if (buttonText.equals("9")){
-				stackData.add(0,9.0);
-				change(stackData);
-				calc.push(9.0);
-				System.out.println(calc.getOperandStack());
+				change("9");
+			} else if (buttonText.equals("0")){
+				change("0");
+			} else if (buttonText.equals(".")){
+				change(".");
+			} else if (buttonText.equals("<-")){
+				Double add = Double.parseDouble(gui.getAccu());
+				calc.push(add);
+				change(calc.getOperandStack());
+				
 			} else if (buttonText.equals("+")) {
 				calc.add();
-				Double somme = calc.pop();
-				stackData.remove(0);
-				stackData.remove(0);
-				stackData.add(0,somme);
-				calc.push(somme);
-				change(stackData);
-				change(Double.toString(calc.getAccumulator()));
+				change(calc.getOperandStack());
+				
 				
 			} else if (buttonText.equals("-")) {
 				calc.subtract();
-				Double diff = calc.pop();
-				stackData.remove(0);
-				stackData.remove(0);
-				stackData.add(0,diff);
-				calc.push(diff);
-				change(stackData);
-				change(Double.toString(calc.getAccumulator()));
+				change(calc.getOperandStack());
 				
 			} else if (buttonText.equals("*")) {
 				calc.multiply();
-				Double produit = calc.pop();
-				stackData.remove(0);
-				stackData.remove(0);
-				stackData.add(0,produit);
-				calc.push(produit);
-				change(stackData);
-				change(Double.toString(calc.getAccumulator()));
+				change(calc.getOperandStack());
 				
 			} else if (buttonText.equals("/")) {
 				calc.divide();
-				Double quotient = calc.pop();
-				stackData.remove(0);
-				stackData.remove(0);
-				stackData.add(0,quotient);
-				calc.push(quotient);
-				change(stackData);
-				change(Double.toString(calc.getAccumulator()));
+				change(calc.getOperandStack());
 				
 			} else if (buttonText.equals("<>")) {
 				calc.swap();
-				stackData.remove(0);
-				stackData.remove(0);
-				Double one = calc.pop();
-				Double two = calc.pop();
-				stackData.add(0,two);
-				stackData.add(0,one);
-				calc.push(two);
-				calc.push(one);
-				change(stackData);
-				System.out.println(calc.getOperandStack());
+				change(calc.getOperandStack());
+				
 			} else if (buttonText.equals("del")){
 				if (!calc.getOperandStack().isEmpty()) {
 					calc.drop();
-					stackData.remove(0);
-					change(stackData);
-					System.out.println(calc.getOperandStack());
-				} else {
-					change("error");
+					change(calc.getOperandStack());
 				}
-			}
+			} else if (buttonText.equals("C")){
+				if (!calc.getOperandStack().isEmpty()) {
+					calc.clearStack();
+					change(calc.getOperandStack());
+				}
+			} 
 		}
 	}
 		
