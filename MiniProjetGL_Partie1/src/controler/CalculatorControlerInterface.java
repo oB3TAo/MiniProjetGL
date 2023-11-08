7189b41c99c22efb8ff69978a1dce620ad07b5d3
loic- <loic-@159.31.71.150>
2023-11-08 14:42:57 +0100

    zClémentz

--- MiniProjetGL_Partie1/src/controler/CalculatorControlerInterface.java
+++ MiniProjetGL_Partie1/src/controler/CalculatorControlerInterface.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public interface CalculatorControlerInterface {
 	
+	/**
+	 * @param accu
+	 */
 	public void change(String accu) ;
+	
+	/**
+	 * @param stackData
+	 */
 	public void change(List<Double> stackData);
+	
+	/**
+	 * 
+	 */
 	public void run();
 	
 }
